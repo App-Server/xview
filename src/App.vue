990328731577175d9764f2e623e40db748f99718
Aragao <epicappcode@gmail.com>
2024-11-17 00:37:44 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,79 @@
+<template>
+  <div>
+    <nav class="navbar navbar-expand-lg bg-darky">
+        <div class="container-md">
+          <a class="navbar-brand" style="color: red;"><h3>Xview</h3></a>
+        </div>
+    </nav>
+    <br>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3 mb-4">
+                <div style="padding:56.27% 0 0 0;position:relative;"><iframe width="3840" height="2160" src="https://www.eporner.com/embed/8H7hZcm9tTZ/" frameborder="0" allowfullscreen style="position:absolute;top:0;left:0;width:100%;height:100%;"></iframe></div>
+            </div>
+
+            <div class="col-md-3">
+                <div style="padding:56.27% 0 0 0;position:relative;"><iframe width="100%" height="100%" src="https://www.eporner.com/embed/rIFiL11K60t/" frameborder="0" allowfullscreen  style="position:absolute;top:0;left:0;width:100%;height:100%;"></iframe></div>
+            </div>
+
+            <div class="col-md-3">
+                <div style="padding:56.27% 0 0 0;position:relative;"><iframe width="1920" height="1080" src="https://www.eporner.com/embed/BzMjsWlN03G/" frameborder="0" allowfullscreen style="position:absolute;top:0;left:0;width:100%;height:100%;"></iframe></div>
+            </div>
+
+            <div class="col-md-3">
+                <div style="padding:56.27% 0 0 0;position:relative;"><iframe width="1280" height="720" src="https://www.eporner.com/embed/bReEOjiDaGR/" frameborder="0" allowfullscreen style="position:absolute;top:0;left:0;width:100%;height:100%;"></iframe></div>
+            </div>
+
+            <div class="col-md-3">
+                <div style="padding:56.27% 0 0 0;position:relative;"><iframe width="1280" height="720" src="https://www.eporner.com/embed/9qis1ARyRgT/" frameborder="0" allowfullscreen style="position:absolute;top:0;left:0;width:100%;height:100%;"></iframe></div>
+            </div>
+
+            <div class="col-md-3">
+                <div style="padding:56.27% 0 0 0;position:relative;"><iframe width="1920" height="1080" src="https://www.eporner.com/embed/lQCq2NZJ9oK/" frameborder="0" allowfullscreen style="position:absolute;top:0;left:0;width:100%;height:100%;"></iframe></div>
+            </div>
+
+            <div class="col-md-3">
+                <div style="padding:56.27% 0 0 0;position:relative;"><iframe width="1920" height="1080" src="https://www.eporner.com/embed/15dJCs0jl8d/" frameborder="0" allowfullscreen style="position:absolute;top:0;left:0;width:100%;height:100%;"></iframe></div>
+            </div>
+ 
+            <div class="col-md-3 mb-4">
+                <div style="padding:56.27% 0 0 0;position:relative;"><iframe width="1920" height="1080" src="https://www.eporner.com/embed/UPCR8N5M8ZH/" frameborder="0" allowfullscreen style="position:absolute;top:0;left:0;width:100%;height:100%;"></iframe></div>
+            </div>
+
+            <div class="col-md-3">
+                <div style="padding:56.27% 0 0 0;position:relative;"><iframe width="1280" height="720" src="https://www.eporner.com/embed/f9zuJZiVeSE/" frameborder="0" allowfullscreen style="position:absolute;top:0;left:0;width:100%;height:100%;"></iframe></div>
+            </div>
+
+            <div class="col-md-3">
+                <div style="padding:56.27% 0 0 0;position:relative;"><iframe width="1920" height="1080" src="https://www.eporner.com/embed/Qj0aTbdoxwd/" frameborder="0" allowfullscreen style="position:absolute;top:0;left:0;width:100%;height:100%;"></iframe></div>
+            </div>
+
+            <div class="col-md-3">
+                <div style="padding:56.27% 0 0 0;position:relative;"><iframe width="3840" height="2160" src="https://www.eporner.com/embed/i6pLoTJagg8/" frameborder="0" allowfullscreen style="position:absolute;top:0;left:0;width:100%;height:100%;"></iframe></div>
+            </div>
+
+            <div class="col-md-3 mb-4">
+                <div style="padding:56.27% 0 0 0;position:relative;"><iframe width="2560" height="1440" src="https://www.eporner.com/embed/w6ASeYCh0dT/" frameborder="0" allowfullscreen style="position:absolute;top:0;left:0;width:100%;height:100%;"></iframe></div>
+            </div>
+
+            <div class="col-md-3">
+                <div style="padding:56.27% 0 0 0;position:relative;"><iframe width="1920" height="1080" src="https://www.eporner.com/embed/XQG8hyG6E3f/" frameborder="0" allowfullscreen style="position:absolute;top:0;left:0;width:100%;height:100%;"></iframe></div>
+            </div>
+
+            <div class="col-md-3">
+                <div style="padding:56.27% 0 0 0;position:relative;"><iframe width="1280" height="720" src="https://www.eporner.com/embed/l7IUcoF7HsF/" frameborder="0" allowfullscreen style="position:absolute;top:0;left:0;width:100%;height:100%;"></iframe></div>
+            </div>
+
+            <div class="col-md-3">
+                <div style="padding:56.27% 0 0 0;position:relative;"><iframe width="1280" height="720" src="https://www.eporner.com/embed/fa0ZjKcHQGs/" frameborder="0" allowfullscreen style="position:absolute;top:0;left:0;width:100%;height:100%;"></iframe></div>
+            </div>
+
+            <div class="col-md-3">
+                <div style="padding:56.27% 0 0 0;position:relative;"><iframe width="1920" height="1080" src="https://www.eporner.com/embed/2gMaFjL7dKc/" frameborder="0" allowfullscreen style="position:absolute;top:0;left:0;width:100%;height:100%;"></iframe></div>
+            </div>
+        </div>
+    </div>
+  </div>
+
+</template>
